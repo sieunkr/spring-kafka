f53688a4d804464bb33a8e57bfc777945b071959
Eddy.Kim <sieunkr@gmail.com>
2019-12-23 22:54:44 +0900

    kafkatemplate 샘플

---
+++ spring-kafka/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        kafkaTemplate.send("peter-topic", "hi");
+    }
+}
